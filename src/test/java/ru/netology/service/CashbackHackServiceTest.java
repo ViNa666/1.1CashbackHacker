82d053236b754c95362c6ee9be94f2511a3756b9
ViNa666 <legenda-2000@yandex.ru>
2022-03-20 16:11:30 +0300

    TestNG tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,56 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldCalculateIfAmountIs0() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual,expected);
+
+    }
+
+    @Test
+    public void shouldCalculateIfAmountIs1To999() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 350;
+
+        int actual = service.remain(amount);
+        int expected = 650;
+
+        assertEquals(actual,expected);
+
+    }
+    @Test
+    public void shouldCalculateIfAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual,expected);
+
+    }
+
+    @Test
+    public void shouldCalculateIfAmountIsMoreThen1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual,expected);
+
+    }
+
+}
